2d94edc2976283e0a6512f2e2d37e41c0558d504
Alexsandro Silva <64186740+alexsandropsilva@users.noreply.github.com>
2023-08-13 21:29:41 -0400

    melhoria do port_scan.py
    aumento de velocidade e eficiência do scan host, port scan e scan network

--- port_scan.py
+++ port_scan.py
@@ -1,48 +1,69 @@
 import socket
-import nmap
+import concurrent.futures
+import ipaddress
+import subprocess
 
-def scan_port(host):
-    open_ports = []
-    closed_ports = []
+def scan_port(host, port):
     try:
-        for port in range(1, 1025):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(0.5)  #Timeout para 0.5 segundos
-            result = sock.connect_ex((host, port))
-            if result == 0:
-                open_ports.append(port)
-            else:
-                closed_ports.append(port)
-            sock.close()
-        print(f"Portas abertas em {host}: {', '.join(map(str, open_ports))}")
-        print(f"Portas fechadas em {host}: {', '.join(map(str, closed_ports[:20]))} ...")  #Mostrando apenas as 20 primeiras portas fechadas para evitar saídas muito longas
-    except KeyboardInterrupt:
-        print("Scanner interrompido pelo usuário")
-        exit()
-    except socket.gaierror:
-        print("O nome do host não pôde ser resolvido")
-        exit()
-    except socket.error:
-        print("Erro ao tentar se conectar ao host")
-        exit()
+        with socket.create_connection((host, port), timeout=0.5):
+            print(f"Porta {port} está aberta em {host}")
+            return True
+    except (socket.timeout, ConnectionRefusedError, OSError):
+        pass
+    return False
 
-def scan_network_range(network_range):
-    nm = nmap.PortScanner()
-    nm.scan(hosts=network_range, arguments='-O')
+def scan_host_ports(host):
+    open_ports = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+        results = list(executor.map(lambda port: scan_port(host, port), range(1, 1025)))
+        open_ports = [port for port, is_open in zip(range(1, 1025), results) if is_open]
+    return open_ports
 
-    for host in nm.all_hosts():
-        ip = nm[host]['addresses']['ipv4']
-        print(f"Host: {host}")
+def scan_network_with_ports(network_range):
+    network = ipaddress.ip_network(network_range, strict=False)
+    for host in network.hosts():
+        if is_host_online(host):
+            open_ports = scan_host_ports(str(host))
+            if open_ports:
+                print(f"Host {host} tem as seguintes portas abertas: {', '.join(map(str, open_ports))}")
+
+def is_host_online(host):
+    try:
+        subprocess.check_output(
+            ['ping', '-c', '1', '-W', '1', str(host)],
+            stderr=subprocess.STDOUT,
+            universal_newlines=True
+        )
+        print(f"Host online: {host}")
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+def scan_network_hosts(network_range):
+    network = ipaddress.ip_network(network_range, strict=False)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+        list(executor.map(is_host_online, network.hosts()))
 
 def main():
-    choice = input("Escolha uma opção:\n1 - Port Scan\n2 - Scan de Rede\nDigite o número da opção: ")
+    print("Escolha uma opção:")
+    print("1 - Scan rápido de porta em um único host")
+    print("2 - Scan da rede com descoberta de portas")
+    print("3 - Verificar hosts ativos na rede")
+    choice = input("Digite o número da opção: ")
 
     if choice == '1':
-        host = input("Digite o IP do host para a varredura de porta: ")
-        scan_port(host)
+        host = input("Digite o IP do host: ")
+        open_ports = scan_host_ports(host)
+        if open_ports:
+            print(f"Host {host} tem as seguintes portas abertas: {', '.join(map(str, open_ports))}")
+        else:
+            print(f"Nenhuma porta aberta encontrada em {host}.")
     elif choice == '2':
         network_range = input("Digite o range da rede (exemplo: 192.168.1.0/24): ")
-        scan_network_range(network_range)
+        scan_network_with_ports(network_range)
+    elif choice == '3':
+        network_range = input("Digite o range da rede (exemplo: 192.168.1.0/24): ")
+        scan_network_hosts(network_range)
     else:
         print("Opção inválida")
 
